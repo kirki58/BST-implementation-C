31e8d025a8e2e64e865dc816ad091879a1bf3260
Burak Kırık <burakkirikk@gmail.com>
2023-09-30 19:48:40 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Binary Search Tree
+I've implemented Binary Search Tree data structure with some handy function in C.
+
+## What are BSTs?
+A Binary Search Tree (BST) is a non-linear hierarchycal tree data structre with some special constraints like every node can have at most 2 children. BSTs are used in various fields and various jobs and can have more then one implementation.
+
+## My functions
+| name       | parameters           | returns | description                                                                                                                                                                       |
+|------------|----------------------|---------|-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| init_node  | int val              | Node*   | allocates memory for a new node and return a pointer to it                                                                                                                        |
+| insert     | Node** root, int val | Node*   | inserts a new node with a key of `val` and returns a pointer to it                                                                                                                |
+| search     | Node* root, int val  | Node*   | searchs for a specific node with value `val` starting from root node, returns a pointer to the found node on the first occurance or null pointer if there is no occurance at all. |
+| isInTree   | Node* root, int val  | bool    | checks if a node with a key of `val` is existent in the tree, returns true if yes. just a shorthand of saying `search(root,val) == NULL`                                          |
+| rightmost  | Node** root          | Node**  | return a pointer to a pointer to the rightmost node in the tree.                                                                                                                  |
+| leftmost   | Node** root          | Node**  | return a pointer to a pointer to the leftmost node in the tree.                                                                                                                   |
+| max        | Node* root           | int     | returns the biggest key in the tree.                                                                                                                                              |
+| min        | Node* root           | int     | returns the smallest key in the tree.                                                                                                                                             |
+| rm_node    | Node** root, int val | void    | removes the first occurance of a node with key `val`.                                                                                                                             |
+| get_height | Node* root           | int     | returns the number of edges from root to the farthest leaf node.                                                                                                                  |
+| get_depth  | Node* root, Node* nd | int     | returns the number of edges from the root node to `nd`                                                                                                                            |
---
+++ bst.c
@@ -0,0 +1,167 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+}Node;
+
+Node* init_node(int val){
+    Node* bstn = (Node*) malloc(sizeof(Node));
+    bstn->data = val;
+    bstn->left = NULL;
+    bstn->right = NULL;
+    return bstn;
+}
+Node* insert(Node** root, int val){
+    Node* newNode = init_node(val);
+    if(*root == NULL){
+        *root = newNode;
+        return *root;
+    }
+    Node* temp = *root;
+    if(val > temp->data){
+        if(temp->right != NULL){
+            insert(&temp->right, val);
+        }
+        else{
+            temp->right = newNode;
+            return newNode;
+        }
+    }
+
+    // if val <= temp->data
+    if(temp->left != NULL){
+        insert(&temp->left,val);
+    }
+    else{
+        temp->left = newNode;
+        return newNode;
+    }
+}
+Node* search(Node* root, int val){
+    if(root == NULL){
+        return NULL;
+    }
+    Node* temp = root;
+    if(temp->data == val){
+        return temp;
+    }
+    else if(temp->data > val){
+        return search(temp->left,val);
+    }
+    else if(temp->data < val){
+        return search(temp->right,val);
+    }
+}
+bool isInTree(Node* root, int val){
+    return(search(root,val) != NULL);
+}
+Node** rightmost(Node** root){
+    if((*root) == NULL){
+        printf("rightmost call on NULL node returned NULL\n");
+        return NULL;
+    }
+    if((*root)->right == NULL){
+        return (root);
+    }
+    else{
+        return rightmost(&((*root)->right));
+    }
+}
+Node** leftmost(Node** root){
+    if((*root) == NULL){
+        printf("leftmost call on NULL node returned NULL\n");
+        return NULL;
+    }
+    if((*root)->left == NULL){
+        return (root);
+    }
+    else{
+        return leftmost(&((*root)->left));
+    }
+}
+int max(Node* root){
+    return( (*rightmost(&root))->data );
+}
+int min(Node* root){
+    return( (*leftmost(&root))->data );
+}
+
+void rm_node(Node** root, int val){
+    if(!isInTree(*root,val)){
+        printf("No value as %d\n",val);
+    }
+    else{
+        if((*root)->data == val){
+            if((*root)->right == NULL && (*root)->left == NULL){
+                *root = NULL;
+                free(*root);
+            }
+            else if((*root)->right == NULL && (*root)->left != NULL){
+                (*root) = (*root)->left; 
+            }
+            else if((*root)->right != NULL && (*root)->left == NULL){
+                (*root) = (*root)->right; 
+            }
+            else{
+                Node** rm = rightmost(&((*root)->right));
+                (*root)->data = (*rm)->data;
+                (*rm) = NULL;
+                free(*rm);
+            }
+            return;
+        }
+        else if(val > (*root)->data){
+            rm_node(&((*root)->right), val);
+        }
+        else{
+            rm_node(&((*root)->left), val);
+        }
+    }
+}
+
+static int maxVal(int i1, int i2){
+    if(i1>=i2){
+        return i1;
+    }
+    else{
+        return i2;
+    }
+}
+static int height(Node* root, int ht){
+    if(root == NULL){
+        return 0;
+    }
+    int h = ht;
+    if(root->right == NULL && root->left == NULL){
+        return h;
+    }
+    return maxVal(height(root->left,h+1),height(root->right,h+1));
+}
+int get_height(Node* root){
+    return height(root,0);
+}
+static int depth(Node* root, Node* nd, int dt){
+    if(!isInTree(root,val)){
+        printf("No node as \n",nd->data);
+        return;
+    }
+    else{
+        int d = dt;
+        if(root == nd){
+            return d;
+        }
+        else if(nd->data > root->data){
+            return depth(root->right,nd,d+1);
+        }
+        else{
+            return depth(root->left,nd,d+1);
+        }
+    }
+}
+int get_depth(Node* root, Node* nd){
+    return depth(root,nd,0);
+}
